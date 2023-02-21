58ba810679eae6b49e41fa0f6051d52259210f5c
chani85103 <a0527616968@gmail.com>
2023-02-21 19:58:01 +0200

    endAndWork

--- ‏‏src - עותק/models/Client.ts
+++
@@ -1,5 +0,0 @@
-import Child from "./Child";
-
-export default class Client{
-    constructor(public Id:string,public FirstName:string,public LastName:string,public BirthDate:Date,public Gender:string,public HMO:string){}
-}
